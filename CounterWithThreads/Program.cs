19ca9c53e2f1febc3a2262e0193b4aa5353fa1b0
Mathias <Mathias@LAPTOP-84H7VQ2I>
2017-01-31 18:28:14 +0100

    Add project files.

---
+++ CounterWithThreads/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CounterWithThreads
+{
+    class Program
+    {
+        static object locker = new object();
+        static int count;
+
+        static void ThreadMain()
+        {
+            WriteCount();
+        }
+
+        static void WriteCount()
+        {
+            string threadName = Thread.CurrentThread.Name;
+
+            while (true)
+            {
+                Monitor.Enter(locker);
+                if (threadName == "firstThread")
+                    count += 2;
+                else
+                    count -= 1;
+
+                Console.WriteLine(count);
+                Monitor.Exit(locker);
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread firstThread = new Thread(WriteCount);
+            Thread secondThread = new Thread(WriteCount);
+            firstThread.Name = "firstThread";
+            secondThread.Name = "secondThread";
+            firstThread.Start();
+            secondThread.Start();
+
+            Console.Read();
+        }
+    }
+}
